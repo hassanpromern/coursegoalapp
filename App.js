dc49117c52d9050dc5c41f035a9f60a4f366e609
hassanpromern <hassanmughal6951@gmail.com>
2021-12-28 13:00:08 +0500

    coursegoalapp

--- App.js
+++ App.js
@@ -1,20 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import GoalItem from "./components/GaolItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [addModal, setaddModal] = useState(false);
+
+  const addGoalHandler = (goalTitle) => {
+    // setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle },
+    ]);
+    setaddModal(false);
+  };
+
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
+  const cancelOnGoalAdditionHandler = () => {
+    setaddModal(false);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title="Add Goal" onPress={() => setaddModal(true)} />
+      <GoalInput
+        visible={addModal}
+        onAddGoal={addGoalHandler}
+        onCancel={cancelOnGoalAdditionHandler}
+      />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
+      />
+      {/* <ScrollView>
+        {courseGoals.map((goal) => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
 });
+
+{
+  /* Felxbox Deep Drive */
+}
+
+{
+  /* 
+      
+      // {
+        //   // flexDirection: "row",
+        //   // width: "80%",
+        //   // height: 300,
+        //   // justifyContent: "space-around",
+        //   // alignItems: "stretch",
+        // }
+      <View
+        style={{
+          backgroundColor: "red",
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>1</Text>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: "blue",
+          flex: 2,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "orange",
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
+      </View> */
+}
